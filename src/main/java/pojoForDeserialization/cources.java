37d4f3737c567ecc00f74816c6308e0de9cdc793
nag <nag@gmai.com>
2020-01-01 00:55:13 +0530

    Pojo classess adedd for deserialization

---
+++ src/main/java/pojoForDeserialization/cources.java
@@ -0,0 +1,30 @@
+package pojoForDeserialization;
+
+import java.util.List;
+
+public class cources {
+private List<webAutomation> webAutomation;
+private List<api> api;
+private List<mobile> mobile;
+
+public List<pojoForDeserialization.webAutomation> getWebAutomation() {
+	return webAutomation;
+}
+public void setWebAutomation(List<pojoForDeserialization.webAutomation> webAutomation) {
+	this.webAutomation = webAutomation;
+}
+public List<pojoForDeserialization.api> getApi() {
+	return api;
+}
+public void setApi(List<pojoForDeserialization.api> api) {
+	this.api = api;
+}
+public List<pojoForDeserialization.mobile> getMobile() {
+	return mobile;
+}
+public void setMobile(List<pojoForDeserialization.mobile> mobile) {
+	this.mobile = mobile;
+}
+
+
+}
